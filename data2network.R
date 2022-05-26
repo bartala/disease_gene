ae23748728b6c66b97eb72b61564344a6b7fa080
Alon Bartal <bartala@gmail.com>
2022-05-26 11:01:31 +0300

    Update data2network.R

--- data2network.R
+++ data2network.R
@@ -3,8 +3,9 @@ library(sqldf)
 library(igraph)
 library(data.table)
 library(foreach)
+library(dplyr)
 
-PTH = "/patg/to/data/" # enter your data path
+PTH = "/path/to/data/" # enter your data path
 
 
 # =========================================================================================================================
@@ -40,15 +41,6 @@ write.csv(g_d_edges,file = paste0(PTH,"g_d_edges.csv"))
 # =========================================================================================================================
 
 # load gene-tissue data from https://www.proteinatlas.org/about/download
-# load gene-tissue data from https://www.proteinatlas.org/about/download
-
-library(readr)
-library(sqldf)
-library(igraph)
-library(data.table)
-library(foreach)
-
-PTH = '/home/bartalab/Desktop/'
 
 normal_tissue_tsv <- read_delim(paste0(PTH,"normal_tissue.tsv.zip"), "\t", escape_double = FALSE, trim_ws = TRUE)
 
@@ -86,20 +78,20 @@ gene_gene_net_edges$V3 <- as.numeric(gene_gene_net_edges$V3)
 write.csv(gene_gene_net_edges,file=paste0(PTH,"gene_gene_net_edges_normal_tissue.csv"))
 
 # =========================================================================================================================
-# STEP3: enrich the gene-disease network with gene-gene edges from Step 2
-# Create a Combined Network (g_d_t.csv) with two types of edges by adding gene-gene edges in Step #2 into the gene-disease
-# network in Step #1
+# STEP3: combine the gene-disease graph with gene-gene edges from Step 2
+# Create a Combined graph (g_d_t.csv) with two types of edges by adding gene-gene edges in Step #2 into the gene-disease
+# graph in Step #1
 # =========================================================================================================================
 
 g_d_edges<-fread(paste0(PTH,"g_d_edges.csv"),header=TRUE)
 g_d_edges$V1<-NULL
-g_d_edges$type<-1  # 13k unique genes
+g_d_edges$type<-1
 g_d_edges<-data.frame(g_d_edges)
 
 
 gene_gene <- fread(paste0(PTH,"gene_gene_net_edges_normal_tissue.csv"),header=TRUE)
 gene_gene$V1<-NULL
-names(gene_gene)<-c("from","to","weight") # 1659 unique genes
+names(gene_gene)<-c("from","to","weight")
 g_g_t$type<-2
 
 # from g_g_t (tissue info) edges, keep only edges that their nodes (genes) are in gene-disease (g_d_edges)
@@ -116,8 +108,7 @@ write.csv(g_d_t,paste0(PTH,"g_d_t.csv"))
 # STEP4: Create positive (`POS`) and negative (`NEG`) examples.
 # =========================================================================================================================
 
-# 4.1 `POS` Examples: sample 20% of *gene-disease* edges (type 1) and delete them from the original network (g_d_t.csv) 
-# while ensuring that the original network obtained after edge removals is connected
+# Positive (`POS`) Examples: sample 20% of *gene-disease* edges (type 1) and delete them from the original network (g_d_t.csv) 
 
 g_d_t <- fread(paste0(PTH,'g_d_t.csv'))
 g_d_t$V1<-NULL
@@ -126,12 +117,10 @@ gene_disease <- g_d_t[g_d_t$type==1,c(1,2)]
 gene_disease<-data.frame(gene_disease)
 names(gene_disease)<-c("gene","disease")
 
-g<-graph_from_data_frame(gene_disease,directed = FALSE)
-diseases<-unique(gene_disease$disease)
-clu <- components(g)
+diseases <- unique(gene_disease$disease)
 
-# sample 20% positive edges
-smpedges<-data.frame()
+# sample 20% of edges as positive examples
+pos_edges<-data.frame()
 
 for(disease in diseases){
   print(disease)
@@ -139,73 +128,97 @@ for(disease in diseases){
   N = round(0.2*nrow(df))
   # sample gene-edges
   edges<-df[sample(1:nrow(df),N),] # sample 20% edge
-  smpedges <- rbind(smpedges,edges)
+  pos_edges <- rbind(pos_edges,edges)
 }
 
-length(unique(smpedges$disease))
+# number of diseases sampled
+length(unique(pos_edges$disease))
 
-# delete pos edges from the graph to create train edges for the N2V embeddings
-library(dplyr)
-new_edges <- anti_join(gene_disease, smpedges,by=c("gene","disease"))
-
-pos_edges <- smpedges
+# delete `pos` edges from the graph G to create train edges for node2vec embeddings
 names(pos_edges)<-c("from","to")
-dis<-anti_join(g_d_t, pos_edges, by=c("from","to"))
+edges_G_tag<-anti_join(g_d_t, pos_edges, by=c("from","to"))
 
-write.csv(dis,file=paste0(PTH,"/new_network.csv"))
 
-write.csv(pos_edges,file=paste0(PTH,"pos_edges.csv"))
+write.csv(edges_G_tag,file=paste0(PTH,"G_tag.csv"), row.names = FALSE)
+write.csv(pos_edges,file=paste0(PTH,"pos_edges.csv"), row.names = FALSE)
 
 
 # `NEG`: sample the same number as `POS` examples - randomly select pairs of nodes that have no edges in Step 3.
-#* 50% of gene-gene edges
-#* 50% of gene-disease edges
+#(1) 50% of gene-gene edges
+#(2) 50% of gene-disease edges with low GDPS
 
-#* 50% of gene-disease edges
 N = round(nrow(pos_edges)/2)
+
+# (1) ---------- 50% of negative gene-gene edges ----------
+
 genes<- unique(gene_disease$gene)
-neg_edges <- data.frame( from=sample(genes, N,replace = TRUE), to=sample(diseases, N,replace = TRUE) )
-# keep only non existing edges
-neg_edges <- anti_join(neg_edges,g_d_t, by=c("from","to"))
 
-# check that NEG edges do not exist in real edges
-tmp<-data.frame()
-while(N-nrow(neg_edges)>nrow(tmp)+1){
-  n = N-nrow(neg_edges)-nrow(tmp)
-  tmp<-data.frame( from=sample(genes, n,replace = TRUE), to=sample(diseases, n,replace = TRUE) )
-  tmp <- anti_join(tmp,g_d_t, by=c("from","to"))
-  tmp <- anti_join(tmp,neg_edges, by=c("from","to"))
-}
-neg_edges<-rbind(neg_edges,tmp)
-neg_edges<-neg_edges[union(neg_edges$from,neg_edges$to) %in% union(dis$from,dis$to),]
-neg_edges <- anti_join(neg_edges,g_d_t, by=c("from","to"))
+neg_gene_gene_edges<-data.frame(
+                                    from = sample(genes, N, replace = TRUE), 
+                                    to = sample(genes, N, replace = TRUE) 
+                                )
 
-
-# 50% of gene-gene edges
-neg_gene_gene_edges<-data.frame( from=sample(genes, N,replace = TRUE), to=sample(genes, N,replace = TRUE) )
+# keep only non existing gene-gene edges
 neg_gene_gene_edges <- anti_join(neg_gene_gene_edges,g_d_t, by=c("from","to"))
-neg_gene_gene_edges <- anti_join(neg_gene_gene_edges,neg_edges, by=c("from","to"))
 n = N-nrow(neg_gene_gene_edges)
 
-tmp<-data.frame( from=sample(genes, n,replace = TRUE), to=sample(genes, n,replace = TRUE) )
-tmp <- anti_join(tmp,g_d_t, by=c("from","to"))
-tmp <- anti_join(tmp,neg_edges, by=c("from","to"))
-neg_gene_gene_edges <- rbind(neg_gene_gene_edges,tmp)
 
-neg_edges<-rbind(neg_edges,neg_gene_gene_edges)
+# (2)---------- 50% of negative gene-disease edges ---------->>>>>>>>
+
+edges_G_tag <- read_csv(paste0(PTH,"G_tag.csv"))
+
+#---- make sure we sample only low GDPS gene-disease scores ----
+GDPS <- as.matrix(fread(file = paste0(PTH,"disease_prediction.tsv"), sep = "\t"),rownames=1) # gene X disease matrix
+GDPS1 <- GDPS[,colnames(GDPS) %in% edges_G_tag$to] # keep only diseases in G'
+GDPS1 <- GDPS1[rownames(GDPS1) %in% edges_G_tag$from, ] # keep only genes in G'
+rm(GDPS)
+
+diseases <- colnames(GDPS1)
+
+# sample 20% of edges as positive examples
+neg_edges<-data.frame()
+i = 1
+k=1
+while(i < n){
+  j = i
+  if( i > length(diseases)){
+    j = 1
+  }
+  disease = diseases[j]
+  ordered_genes <- rownames(data.frame(sort((GDPS1[,disease]),decreasing = FALSE))) # get genes with low GDPS score
+  df<- g_d_t[g_d_t$from == ordered_genes[k] & g_d_t$to == disease, ]
+  if( nrow(df) == 0 ){
+    neg_edges <- rbind(
+                        neg_edges,
+                        c(ordered_genes[1],disease)
+                      )
+    i=i+1
+    print(i)
+  }
+  else{
+    k=k+1
+  }
+}
+
+# make sure only non existing edges are in neg_edges
+names(neg_edges) <- c("from","to")
 neg_edges <- anti_join(neg_edges,g_d_t, by=c("from","to"))
-neg_edges<-neg_edges[union(neg_edges$from,neg_edges$to) %in% union(dis$from,dis$to),]
 
-write.csv(neg_edges,file=paste0(PTH,"neg_edges.csv"))
+#---------- combine (gene,gene) and (gene, disease) pairs ----------
+neg_edges<-rbind(neg_edges,neg_gene_gene_edges)
+
+# make sure all genes and diseases are in G'
+neg_edges<-neg_edges[union(neg_edges$from,neg_edges$to) %in% union(edges_G_tag$from,edges_G_tag$to),]
+
+# make sure negative edges are not in G (`g_d_t`)
+neg_edges <- anti_join(neg_edges,g_d_t, by=c("from","to"))
+
+write.csv(neg_edges,file=paste0(PTH,"neg_edges.csv"), row.names = FALSE)
 
 
-#4.3 Shuffle and split the examples (`training_testing_edges.csv`) into training (70%) and testing (30%)
-neg_edges<-neg_edges <- read_csv(paste0(PTH,"neg_edges.csv"), col_types = cols(X1 = col_skip()))
-pos_edges<-pos_edges <- read_csv(paste0(PTH,"pos_edges.csv"), col_types = cols(X1 = col_skip()))
+#---------- combine pos and neg examples ----------
 neg_edges$type<-0
 pos_edges$type<-1
 names(pos_edges)<-names(neg_edges)
 pos_neg_edges<-rbind(neg_edges,pos_edges)
-rows <- sample(nrow(pos_neg_edges))
-df <- pos_neg_edges[rows, ]
-write.csv(df,file=paste0(PTH,"pos_neg_edges.csv"))
+write.csv(pos_neg_edges,file=paste0(PTH,"pos_neg_edges.csv"), row.names = FALSE)
