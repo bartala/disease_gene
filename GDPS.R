c7bb7efaf9c8479df832242a326ee18977528b00
Alon Bartal <bartala@gmail.com>
2022-05-29 15:28:27 +0300

    Update GDPS.R

--- GDPS.R
+++ GDPS.R
@@ -5,36 +5,44 @@ library(dplyr)
 
 PTH = "/path/to/data"
 
+
 # ------------------------ load data -------------------------------------------------------------
 
 # Load G_tag
-G <- read_csv(paste0(PTH,"g_d_t.csv"))
+G <- read_csv(paste0(PTH,"g_d_t.csv"))[,-1]
 G <- G[G$type == 1,]
-names(G) = c("gene","disease")
+names(G) = c("gene","disease","label")
 
 
 # load human gene-gene correlation matrix from Archs4
 load(paste0(PTH,"human_correlation.rda"))
-genes <- row.names(cc)
+genes_archs4 <- names(dataset)
 
 # ------------------------  GDPS -------------------------------------------------------------
 
-GDPS <- function(disease){         
-          z_tag <-  G_tag[ , G_tag$disease == disease,]$genes ] # select gene columns in cc
-          GDPS_vec <- rowMeans(z_tag)
-          return(GDPS_vec)
-}
+GDPS <- function(disease){    
+                    genes <- unique(G[G$disease == disease,]$gene)
+                    genes <- genes[genes %in% genes_archs4]
+                    z_tag <-  dataset[ ,genes] # select gene columns of the gene-gene matrix
+                    GDPS_vec <- rowMeans(z_tag)
+                    return(GDPS_vec)
+                  }
 
 
 
 diseaeses <- unique(G$disease)
 
-GDPS_matrix = data.frame()
-
+GDPS_matrix <- list()
+i = 1
 for(disease in diseaeses){
   
-        GDPS_vec <- GDPS(disease)
-        GDPS_matrix <- cbind(GDPS_matrix,GDPS)
+  GDPS_matrix[[i]] <- GDPS(disease)
+  i <- i+1
+  print(i)
+  
 }
+
+mydf <- data.frame(GDPS_matrix)
+row.names(mydf)<-genes_archs4
 
 write.csv(GDPS_matrix, file = paste0(PTH,"gene_disease_GDPS_matrix"))
